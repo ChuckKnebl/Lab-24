9fa5aa70a51891fe98f0ed33a5fc10596eea8e4f
Chuck Knebl <chuck.knebl@att.net>
2017-05-02 23:07:24 -0400

    Commit for Lab 24

---
+++ LAB-23-CK/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LAB_23_CK
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LAB-23-CK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LAB_23_CK.Startup))]
+namespace LAB_23_CK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
